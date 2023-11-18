c294e7162df67409a1b0ec87cb62a3d993e746ad
Jungma <mbjun3630@naver.com>
2023-11-18 23:43:34 +0900

    AppBar 디자인

--- web_view/lib/screen/home_screen.dart
+++ web_view/lib/screen/home_screen.dart
@@ -5,6 +5,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text('Code Factory'),
+        centerTitle: true,
+      ),
+    );
   }
 }
