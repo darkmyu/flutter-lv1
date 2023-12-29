887a582adfe7c6a2f393e5b2cbd3d8778b8c9bac
Jungma <mbjun3630@naver.com>
2023-12-29 22:54:39 +0900

    HomeScreen 초기 구현

---
+++ vid_player/lib/screen/home_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: getBoxDecoration(),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _Logo(),
+            SizedBox(
+              height: 30.0,
+            ),
+            _AppName(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration getBoxDecoration() {
+    return const BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color(0xFF2A3A7C),
+          Color(0xFF000118),
+        ],
+      ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'asset/image/logo.png',
+    );
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName({super.key});
+
+  final textStyle = const TextStyle(
+    color: Colors.white,
+    fontSize: 30.0,
+    fontWeight: FontWeight.w300,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          'VIDEO',
+          style: textStyle,
+        ),
+        Text(
+          'PLAYER',
+          style: textStyle.copyWith(
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ],
+    );
+  }
+}
