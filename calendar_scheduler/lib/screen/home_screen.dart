4a9495d10c77daebb9e70fa8c25c33f6e7b0f189
bammyu <mbjun3630@naver.com>
2024-05-05 18:33:39 +0900

    BottomSheet 디자인 마무리

--- calendar_scheduler/lib/screen/home_screen.dart
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:calendar_scheduler/component/calendar.dart';
-import 'package:calendar_scheduler/component/custom_text_field.dart';
+import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
 import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:calendar_scheduler/constant/colors.dart';
@@ -23,68 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
         onPressed: () {
           showModalBottomSheet(
             context: context,
-            builder: (_) {
-              return SafeArea(
-                bottom: true,
-                child: Container(
-                  color: Colors.white,
-                  height: 600,
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
-                    child: Column(
-                      children: [
-                        const Row(
-                          children: [
-                            Expanded(
-                              child: CustomTextField(
-                                label: '시작 시간',
-                              ),
-                            ),
-                            SizedBox(width: 16.0),
-                            Expanded(
-                              child: CustomTextField(
-                                label: '마감 시간',
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 8.0),
-                        const Expanded(
-                          child: CustomTextField(
-                            label: '내용',
-                            expand: true,
-                          ),
-                        ),
-                        const SizedBox(height: 8.0),
-                        Row(
-                          children: categoryColors
-                              .map(
-                                (e) => Padding(
-                                  padding: const EdgeInsets.only(right: 8.0),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      color: Color(
-                                        int.parse(
-                                          'FF$e',
-                                          radix: 16,
-                                        ),
-                                      ),
-                                      shape: BoxShape.circle,
-                                    ),
-                                    width: 32.0,
-                                    height: 32.0,
-                                  ),
-                                ),
-                              )
-                              .toList(),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            },
+            builder: (_) => const ScheduleBottomSheet(),
           );
         },
         backgroundColor: primaryColor,
