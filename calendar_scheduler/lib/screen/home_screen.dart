043783973a9269ea6a5d4b2ea402f4c7bcc20aa9
bammyu <mbjun3630@naver.com>
2024-05-05 16:48:30 +0900

    카테고리 색상 렌더링

--- calendar_scheduler/lib/screen/home_screen.dart
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -24,35 +24,63 @@ class _HomeScreenState extends State<HomeScreen> {
           showModalBottomSheet(
             context: context,
             builder: (_) {
-              return Container(
-                color: Colors.white,
-                height: 600,
-                child: const Padding(
-                  padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Expanded(
-                            child: CustomTextField(
-                              label: '시작 시간',
+              return SafeArea(
+                bottom: true,
+                child: Container(
+                  color: Colors.white,
+                  height: 600,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
+                    child: Column(
+                      children: [
+                        const Row(
+                          children: [
+                            Expanded(
+                              child: CustomTextField(
+                                label: '시작 시간',
+                              ),
                             ),
-                          ),
-                          SizedBox(width: 16.0),
-                          Expanded(
-                            child: CustomTextField(
-                              label: '마감 시간',
+                            SizedBox(width: 16.0),
+                            Expanded(
+                              child: CustomTextField(
+                                label: '마감 시간',
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      Expanded(
-                        child: CustomTextField(
-                          label: '내용',
-                          expand: true,
+                          ],
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 8.0),
+                        const Expanded(
+                          child: CustomTextField(
+                            label: '내용',
+                            expand: true,
+                          ),
+                        ),
+                        const SizedBox(height: 8.0),
+                        Row(
+                          children: categoryColors
+                              .map(
+                                (e) => Padding(
+                                  padding: const EdgeInsets.only(right: 8.0),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Color(
+                                        int.parse(
+                                          'FF$e',
+                                          radix: 16,
+                                        ),
+                                      ),
+                                      shape: BoxShape.circle,
+                                    ),
+                                    width: 32.0,
+                                    height: 32.0,
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               );
