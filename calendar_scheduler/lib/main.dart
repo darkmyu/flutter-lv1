0100982622453aa3bfe9e0f3dd1fe2e4b409f1e5
bammyu <mbjun3630@naver.com>
2024-06-27 23:28:59 +0900

    SQL 메서드 테스트

--- calendar_scheduler/lib/main.dart
+++ calendar_scheduler/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:calendar_scheduler/constant/colors.dart';
+import 'package:calendar_scheduler/database/drift.dart';
 import 'package:calendar_scheduler/screen/home_screen.dart';
+import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
@@ -6,6 +9,21 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting();
+
+  final database = AppDatabase();
+
+  await database.createSchedule(
+    ScheduleTableCompanion(
+      startTime: const Value(12),
+      endTime: const Value(12),
+      content: const Value('Flutter Study'),
+      date: Value(DateTime.utc(2024, 6, 27)),
+      color: Value(categoryColors.first),
+    ),
+  );
+
+  final response = await database.getSchedules();
+  print(response);
 
   runApp(
     MaterialApp(
