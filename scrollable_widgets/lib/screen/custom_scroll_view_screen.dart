4400cb5e30f74e75739a50d56701ca918281c0db
bammyu <mbjun3630@naver.com>
2024-07-30 22:53:04 +0900

    CustomScrollView - List 위젯

---
+++ scrollable_widgets/lib/screen/custom_scroll_view_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widgets/const/colors.dart';
+
+class CustomScrollViewScreen extends StatelessWidget {
+  final List<int> numbers = List.generate(100, (index) => index);
+
+  CustomScrollViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBar(
+            title: Text(
+              'CustomScrollViewScreen',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            backgroundColor: Colors.blue,
+          ),
+          renderBuilderSliverList(),
+        ],
+      ),
+    );
+  }
+
+  // 1. SliverChildListDelegate - 전부 렌더링 (성능 이슈)
+  // ㄴ ListView() 와 유사함
+  SliverList renderChildSliverList() {
+    return SliverList(
+      delegate: SliverChildListDelegate(
+        numbers
+            .map(
+              (e) => renderContainer(
+                color: rainbowColors[e % rainbowColors.length],
+                index: e,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+
+  // 2. SliverChildBuilderDelegate - 리스트 가상화 (성능 향상)
+  // ㄴ ListView.builder() 와 유사함
+  SliverList renderBuilderSliverList() {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return renderContainer(
+            color: rainbowColors[index % rainbowColors.length],
+            index: index,
+          );
+        },
+        childCount: 100,
+      ),
+    );
+  }
+
+  Widget renderContainer({
+    required Color color,
+    required int index,
+    double? height,
+  }) {
+    print(index);
+
+    return Container(
+      height: height ?? 300,
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
