c4bd53d20c23da2a24071f0fc22d79596cd0f145
bammyu <mbjun3630@naver.com>
2024-08-02 22:50:06 +0900

    CustomScrollView - AppBar 위젯

--- scrollable_widgets/lib/screen/custom_scroll_view_screen.dart
+++ scrollable_widgets/lib/screen/custom_scroll_view_screen.dart
@@ -11,21 +11,48 @@ class CustomScrollViewScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          const SliverAppBar(
-            title: Text(
-              'CustomScrollViewScreen',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            backgroundColor: Colors.blue,
-          ),
+          renderSliverAppBar(),
           renderBuilderSliverList(),
           renderSliverGridBuilder(),
         ],
       ),
+    );
+  }
+
+  SliverAppBar renderSliverAppBar() {
+    return SliverAppBar(
+      // 스크롤 했을때 리스트 중간 이어도 AppBar 내려옴
+      floating: true,
+      // AppBar 상단 고정
+      pinned: false,
+      // 자석 효과
+      // ㄴ floating: true 에만 사용 가능
+      snap: true,
+      // 상단 스크롤 최대시 AppBar 차지
+      stretch: true,
+      expandedHeight: 200,
+      collapsedHeight: 150,
+      flexibleSpace: FlexibleSpaceBar(
+        title: const Text(
+          'FlexibleSpace',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        background: Image.asset(
+          'asset/img/image_1.jpeg',
+          fit: BoxFit.cover,
+        ),
+      ),
+      title: const Text(
+        'CustomScrollViewScreen',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      backgroundColor: Colors.blue,
     );
   }
 
