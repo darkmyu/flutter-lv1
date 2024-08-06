ab7d340bfbdb580f8d4484945f31187654a1077e
bammyu <mbjun3630@naver.com>
2024-08-07 00:05:24 +0900

    Dio 라이브러리 HTTP 요청 테스트

--- dusty_dust/lib/screen/home_screen.dart
+++ dusty_dust/lib/screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:dusty_dust/component/category_stat.dart';
 import 'package:dusty_dust/component/hourly_stat.dart';
 import 'package:dusty_dust/component/main_stat.dart';
 import 'package:dusty_dust/const/colors.dart';
+import 'package:dusty_dust/repository/stat_repository.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,15 +10,24 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: primaryColor,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            MainStat(),
-            CategoryStat(),
-            HourlyStat(),
-          ],
+        child: FutureBuilder(
+          future: StatRepository.fetchData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              print(snapshot.data!['response']['body']['items']);
+            }
+
+            return const Column(
+              children: [
+                MainStat(),
+                CategoryStat(),
+                HourlyStat(),
+              ],
+            );
+          },
         ),
       ),
     );
