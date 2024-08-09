41b87e6a10455206ba757456b155faf73bda33b9
bammyu <mbjun3630@naver.com>
2024-08-09 23:08:45 +0900

    JSON 반환값 > StatModel 변환

--- dusty_dust/lib/repository/stat_repository.dart
+++ dusty_dust/lib/repository/stat_repository.dart
@@ -1,7 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:dusty_dust/model/stat_model.dart';
 
 class StatRepository {
-  static Future<Map<String, dynamic>> fetchData() async {
+  static Future<List<StatModel>> fetchData({
+    required ItemCode itemCode,
+  }) async {
+    final itemCodeStr = itemCode == ItemCode.PM25 ? 'PM2.5' : itemCode.name;
+
     final response = await Dio().get(
       'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst',
       queryParameters: {
@@ -9,12 +14,45 @@ class StatRepository {
         'returnType': 'json',
         'numOfRows': 100,
         'pageNo': 1,
-        'itemCode': 'PM10',
+        'itemCode': itemCodeStr,
         'dataGubun': 'HOUR',
         'searchCondition': 'WEEK',
       },
     );
 
-    return response.data;
+    final rawItemsList = response.data['response']['body']['items'];
+
+    List<StatModel> stats = [];
+
+    final List<String> skipKeys = [
+      'dataGubun',
+      'dataTime',
+      'itemCode',
+    ];
+
+    for (Map<String, dynamic> item in rawItemsList) {
+      final dateTime = item['dataTime'];
+
+      for (String key in item.keys) {
+        if (skipKeys.contains(key)) {
+          continue;
+        }
+
+        final regionStr = key;
+        final stat = item[regionStr];
+
+        stats = [
+          ...stats,
+          StatModel(
+            region: Region.values.firstWhere((e) => e.name == regionStr),
+            stat: double.parse(stat),
+            itemCode: itemCode,
+            dateTime: DateTime.parse(dateTime),
+          ),
+        ];
+      }
+    }
+
+    return stats;
   }
 }
--- dusty_dust/lib/screen/home_screen.dart
+++ dusty_dust/lib/screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:dusty_dust/component/category_stat.dart';
 import 'package:dusty_dust/component/hourly_stat.dart';
 import 'package:dusty_dust/component/main_stat.dart';
 import 'package:dusty_dust/const/colors.dart';
+import 'package:dusty_dust/model/stat_model.dart';
 import 'package:dusty_dust/repository/stat_repository.dart';
 import 'package:flutter/material.dart';
 
@@ -13,13 +14,14 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: primaryColor,
       body: SingleChildScrollView(
-        child: FutureBuilder(
-          future: StatRepository.fetchData(),
+        child: FutureBuilder<List<StatModel>>(
+          future: StatRepository.fetchData(
+            itemCode: ItemCode.PM10,
+          ),
           builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              print(snapshot.data!['response']['body']['items']);
-            }
-
+            print(snapshot.error);
+            print(snapshot.stackTrace);
+            print(snapshot.data);
             return const Column(
               children: [
                 MainStat(),
