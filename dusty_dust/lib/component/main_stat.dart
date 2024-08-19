52445dd27c3cc6325e3d6e6cd8a0c1774d4a63f2
bammyu <mbjun3630@naver.com>
2024-08-19 21:41:28 +0900

    CustomScrollView 로 변환

--- dusty_dust/lib/component/main_stat.dart
+++ dusty_dust/lib/component/main_stat.dart
@@ -7,10 +7,14 @@ import 'package:isar/isar.dart';
 
 class MainStat extends StatelessWidget {
   final Region region;
+  final Color primaryColor;
+  final bool isExpanded;
 
   const MainStat({
     super.key,
     required this.region,
+    required this.primaryColor,
+    required this.isExpanded,
   });
 
   @override
@@ -20,72 +24,84 @@ class MainStat extends StatelessWidget {
       fontSize: 40.0,
     );
 
-    return SafeArea(
-      child: SizedBox(
-        width: double.infinity,
-        child: FutureBuilder<StatModel?>(
-            future: GetIt.I<Isar>()
-                .statModels
-                .filter()
-                .regionEqualTo(region)
-                .itemCodeEqualTo(ItemCode.PM10)
-                .sortByDateTimeDesc()
-                .findFirst(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData &&
-                  snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              }
+    return SliverAppBar(
+      backgroundColor: primaryColor,
+      expandedHeight: 500,
+      pinned: true,
+      title: !isExpanded ? Text(region.krName) : null,
+      flexibleSpace: FlexibleSpaceBar(
+        background: SafeArea(
+          child: SizedBox(
+            width: double.infinity,
+            child: FutureBuilder<StatModel?>(
+              future: GetIt.I<Isar>()
+                  .statModels
+                  .filter()
+                  .regionEqualTo(region)
+                  .itemCodeEqualTo(ItemCode.PM10)
+                  .sortByDateTimeDesc()
+                  .findFirst(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData &&
+                    snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                }
 
-              if (!snapshot.hasData) {
-                return const Center(
-                  child: Text('데이터가 없습니다.'),
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: Text('데이터가 없습니다.'),
+                  );
+                }
+
+                final statModel = snapshot.data!;
+                final status = StatusUtils.getStatusModelFromStat(
+                  statModel: statModel,
                 );
-              }
 
-              final statModel = snapshot.data!;
-              final status = StatusUtils.getStatusModelFromStat(
-                statModel: statModel,
-              );
-
-              return Column(
-                children: [
-                  Text(
-                    region.krName,
-                    style: ts.copyWith(
-                      fontWeight: FontWeight.w700,
+                return Column(
+                  children: [
+                    const SizedBox(
+                      height: kToolbarHeight,
                     ),
-                  ),
-                  Text(
-                    DateUtils.DateTimeToString(
-                      dateTime: statModel.dateTime,
+                    Text(
+                      region.krName,
+                      style: ts.copyWith(
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
-                    style: ts.copyWith(
-                      fontSize: 20.0,
+                    Text(
+                      DateUtils.DateTimeToString(
+                        dateTime: statModel.dateTime,
+                      ),
+                      style: ts.copyWith(
+                        fontSize: 20.0,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 20.0),
-                  Image.asset(
-                    status.imagePath,
-                    width: MediaQuery.of(context).size.width / 2,
-                  ),
-                  const SizedBox(height: 20.0),
-                  Text(
-                    status.label,
-                    style: ts.copyWith(
-                      fontWeight: FontWeight.w700,
+                    const SizedBox(height: 20.0),
+                    Image.asset(
+                      status.imagePath,
+                      width: MediaQuery.of(context).size.width / 2,
                     ),
-                  ),
-                  Text(
-                    status.comment,
-                    style: ts.copyWith(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w700,
+                    const SizedBox(height: 20.0),
+                    Text(
+                      status.label,
+                      style: ts.copyWith(
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
-                  ),
-                ],
-              );
-            }),
+                    Text(
+                      status.comment,
+                      style: ts.copyWith(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
       ),
     );
   }
