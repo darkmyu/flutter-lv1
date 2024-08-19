7f0037137115db8247577bd440761c66d9e2087a
bammyu <mbjun3630@naver.com>
2024-08-19 23:41:28 +0900

    기본 TabBar

--- tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
+++ tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:tabbar_theory/const/tabs.dart';
 
 class BasicAppbarTabbarScreen extends StatelessWidget {
   const BasicAppbarTabbarScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [],
+    return DefaultTabController(
+      length: TABS.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('BasicAppBarScreen'),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(80),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TabBar(
+                  indicatorColor: Colors.red,
+                  indicatorWeight: 4,
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  isScrollable: true,
+                  labelColor: Colors.blue,
+                  unselectedLabelColor: Colors.grey,
+                  labelStyle: const TextStyle(
+                    fontWeight: FontWeight.w700,
+                  ),
+                  unselectedLabelStyle: const TextStyle(
+                    fontWeight: FontWeight.w100,
+                  ),
+                  tabs: TABS
+                      .map(
+                        (e) => Tab(
+                          icon: Icon(e.icon),
+                          child: Text(e.label),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: TabBarView(
+          children: TABS
+              .map(
+                (e) => Center(
+                  child: Icon(e.icon),
+                ),
+              )
+              .toList(),
+        ),
       ),
     );
   }
