38c4450be3813ef91241d61e68e07e875c3d9aaf
bammyu <mbjun3630@naver.com>
2024-08-20 00:02:56 +0900

    TabController 사용

---
+++ tabbar_theory/lib/screen/appbar_using_controller_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:tabbar_theory/const/tabs.dart';
+
+class AppbarUsingControllerScreen extends StatefulWidget {
+  const AppbarUsingControllerScreen({super.key});
+
+  @override
+  State<AppbarUsingControllerScreen> createState() =>
+      _AppbarUsingControllerScreenState();
+}
+
+class _AppbarUsingControllerScreenState
+    extends State<AppbarUsingControllerScreen> with TickerProviderStateMixin {
+  late final TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    tabController = TabController(
+      length: TABS.length,
+      vsync: this,
+    );
+
+    tabController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AppBar Using Controller Screen'),
+        bottom: TabBar(
+          controller: tabController,
+          tabs: TABS
+              .map(
+                (e) => Tab(
+                  icon: Icon(e.icon),
+                  child: Text(e.label),
+                ),
+              )
+              .toList(),
+        ),
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: TABS
+            .map(
+              (e) => Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(e.icon),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      if (tabController.index != 0)
+                        ElevatedButton(
+                          onPressed: () {
+                            tabController.animateTo(tabController.index - 1);
+                          },
+                          child: const Text('이전'),
+                        ),
+                      const SizedBox(width: 16),
+                      if (tabController.index != TABS.length - 1)
+                        ElevatedButton(
+                          onPressed: () {
+                            tabController.animateTo(tabController.index + 1);
+                          },
+                          child: const Text('다음'),
+                        ),
+                    ],
+                  )
+                ],
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
--- tabbar_theory/lib/screen/home_screen.dart
+++ tabbar_theory/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tabbar_theory/screen/appbar_using_controller_screen.dart';
 import 'package:tabbar_theory/screen/basic_appbar_tabbar_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -24,6 +25,16 @@ class HomeScreen extends StatelessWidget {
                 );
               },
               child: const Text('Basic AppBar TabBar Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => const AppbarUsingControllerScreen(),
+                  ),
+                );
+              },
+              child: const Text('AppBar Using Controller Screen'),
             ),
           ],
         ),
